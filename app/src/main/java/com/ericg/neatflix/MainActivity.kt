b3de7095bc2b9b52a0eda0a36918909b1fe65a1d
Ericgacoki <gacokieric@gmail.com>
2022-02-12 01:18:20 +0300

    Call the SplashScreen composable

--- app/src/main/java/com/ericg/neatflix/MainActivity.kt
+++ app/src/main/java/com/ericg/neatflix/MainActivity.kt
@@ -3,41 +3,17 @@ package com.ericg.neatflix
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.ericg.neatflix.screens.SplashScreen
 import com.ericg.neatflix.ui.theme.NeatFlixTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             NeatFlixTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                SplashScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    NeatFlixTheme {
-        Greeting("Android")
     }
 }
