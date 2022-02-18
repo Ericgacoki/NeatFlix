4f163a48589d3cefff0a23dc142cab2204cd566d
Ericgacoki <gacokieric@gmail.com>
2022-02-18 14:58:30 +0300

    Add custom colors

--- app/src/main/java/com/ericg/neatflix/ui/theme/Color.kt
+++ app/src/main/java/com/ericg/neatflix/ui/theme/Color.kt
@@ -1,8 +1,13 @@
 package com.ericg.neatflix.ui.theme
 
+import androidx.compose.material.ButtonColors
 import androidx.compose.ui.graphics.Color
 
 val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val AppPrimaryColor = Color(0xFF180E36)
+val AppOnPrimaryColor = Color.White.copy(alpha = 0.78F)
+val ButtonColor = Color(0XFF423460)
