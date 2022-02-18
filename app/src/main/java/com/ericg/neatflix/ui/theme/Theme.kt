da37faf5a8b01072c1f5827e9cd500aaa51688b9
Ericgacoki <gacokieric@gmail.com>
2022-02-18 14:57:02 +0300

    add custom colors

--- app/src/main/java/com/ericg/neatflix/ui/theme/Theme.kt
+++ app/src/main/java/com/ericg/neatflix/ui/theme/Theme.kt
@@ -7,14 +7,14 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
+    primary = AppOnPrimaryColor,
+    primaryVariant = AppOnPrimaryColor,
     secondary = Teal200
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
+    primary = AppPrimaryColor,
+    primaryVariant = AppOnPrimaryColor,
     secondary = Teal200
 
     /* Other default colors to override
