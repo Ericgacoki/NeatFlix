484e9b050950b3d692b45b611b14f73c77017c33
Ericgacoki <gacokieric@gmail.com>
2022-02-19 12:38:40 +0300

    View HomeScreen in MainActivity.kt

--- app/src/main/java/com/ericg/neatflix/screens/MainActivity.kt
+++ app/src/main/java/com/ericg/neatflix/screens/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             NeatFlixTheme {
-                LogInScreen()
+                Home()
             }
         }
     }
