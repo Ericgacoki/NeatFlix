93c66865b64674c3dcad3a2f3a969e70d3e7afff
Ericgacoki <gacokieric@gmail.com>
2022-02-28 02:37:17 +0300

    decrease opacity on app logo

--- app/src/main/java/com/ericg/neatflix/screens/SplashScreen.kt
+++ app/src/main/java/com/ericg/neatflix/screens/SplashScreen.kt
@@ -6,19 +6,18 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
 import com.ericg.neatflix.R
 
@@ -29,54 +28,33 @@ fun SplashScreen() {
             .fillMaxSize()
             .background(Color(0xFF180E36))
     ) {
-        val (banner, logo, title, slogan) = createRefs()
+        val (banner, logo) = createRefs()
 
         Image(
             painter = painterResource(id = R.drawable.movie_banner),
             modifier = Modifier
                 .fillMaxWidth()
                 .fillMaxHeight(0.5F)
+                .clip(RoundedCornerShape(topStart = 28.dp, topEnd = 28.dp))
                 .constrainAs(banner) {
                     top.linkTo(parent.top)
                 },
             contentScale = ContentScale.Crop,
             contentDescription = "Movie banner"
         )
+
         Image(
-            painter = painterResource(id = R.drawable.ic_logo),
             modifier = Modifier
-                //.size(size = 100.dp)
+                .widthIn(max = 150.dp)
+                .alpha(0.78F)
                 .constrainAs(logo) {
                     top.linkTo(banner.bottom)
                     bottom.linkTo(banner.bottom)
-                    start.linkTo(banner.start)
-                    end.linkTo(banner.end)
-                },
-            contentDescription = "App logo"
-        )
-        Text(text = "Neatflix", color = Color(255, 255, 255).copy(alpha = 0.78F),
-            fontSize = 24.sp,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.constrainAs(title) {
-                top.linkTo(logo.bottom, margin = 12.dp)
-                start.linkTo(parent.start)
-                end.linkTo(parent.end)
-            }
-        )
-        val sloganText = "Access unlimited movies, series & TV shows anywhere, anytime"
-        Text(
-            text = sloganText, color = Color(255, 255, 255).copy(alpha = 0.78F),
-            fontSize = 18.sp,
-            fontWeight = FontWeight.Light,
-            modifier = Modifier
-                .constrainAs(slogan) {
-                    top.linkTo(title.bottom, margin = 12.dp)
                     start.linkTo(parent.start)
                     end.linkTo(parent.end)
-                }
-                .padding(horizontal = 4.dp)
-                .fillMaxWidth(),
-            textAlign = TextAlign.Center
+                },
+            painter = painterResource(id = R.drawable.neatflix_logo_large),
+            contentDescription = null
         )
     }
 }
