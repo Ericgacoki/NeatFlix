c8c1cd0fbb722487b330907b256bd8d26f515eeb
Ericgacoki <gacokieric@gmail.com>
2022-02-28 02:36:15 +0300

    Add font and textType for entire app

--- app/src/main/java/com/ericg/neatflix/ui/theme/Type.kt
+++ app/src/main/java/com/ericg/neatflix/ui/theme/Type.kt
@@ -1,18 +1,46 @@
 package com.ericg.neatflix.ui.theme
 
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.ericg.neatflix.R
+
+val quickSandFontFamily = FontFamily(
+    Font(R.font.quicksand_regular, weight = FontWeight.Normal),
+    Font(R.font.quicksand_light, weight = FontWeight.Light),
+    Font(R.font.quicksand_medium, weight = FontWeight.Medium),
+    Font(R.font.quicksand_semibold, weight = FontWeight.SemiBold),
+    Font(R.font.quicksand_bold, weight = FontWeight.Bold),
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = quickSandFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
-    )
+    ),
+    body2 = TextStyle(
+        fontFamily = quickSandFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp
+    ),
+    h1 = TextStyle(
+        fontFamily = quickSandFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 30.sp
+    ),
+    h2 = TextStyle(
+       fontFamily = quickSandFontFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 24.sp
+    ),
+    defaultFontFamily = quickSandFontFamily
+
     /* Other default text styles to override
     button = TextStyle(
         fontFamily = FontFamily.Default,
