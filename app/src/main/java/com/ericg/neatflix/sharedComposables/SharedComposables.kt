4db7d8022d606d11f73cdd1c73b23eba1c0d6ee6
Ericgacoki <gacokieric@gmail.com>
2022-03-03 02:41:44 +0300

    Add reusable composables in this directory

--- app/src/main/java/com/ericg/neatflix/sharedComposables/SharedComposables.kt
+++ app/src/main/java/com/ericg/neatflix/sharedComposables/SharedComposables.kt
@@ -1,19 +1,56 @@
-package com.ericg.neatflix.SharedComposables
+package com.ericg.neatflix.sharedComposables
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.ExposedDropdownMenuDefaults.textFieldColors
 import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.rounded.ArrowBack
 import androidx.compose.material.icons.rounded.ArrowForward
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow.Companion.Ellipsis
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
+import com.ericg.neatflix.R
+import com.ericg.neatflix.data.MovieGenre
+import com.ericg.neatflix.screens.MovieGenreChip
 import com.ericg.neatflix.ui.theme.AppOnPrimaryColor
+import com.ericg.neatflix.ui.theme.AppPrimaryColor
 import com.ericg.neatflix.ui.theme.ButtonColor
+import com.gowtham.ratingbar.RatingBar
+import com.gowtham.ratingbar.RatingBarConfig
+import com.gowtham.ratingbar.RatingBarStyle
+import com.gowtham.ratingbar.StepSize
+import com.skydoves.landscapist.CircularReveal
+import com.skydoves.landscapist.ShimmerParams
+import com.skydoves.landscapist.coil.CoilImage
 
 @Composable
 fun BackOrNextButton(onClick: () -> Unit) {
@@ -33,7 +70,7 @@ fun BackOrNextButton(onClick: () -> Unit) {
                 val (icon) = createRefs()
                 Icon(
                     imageVector = Icons.Rounded.ArrowForward,
-                    contentDescription = "Icon next",
+                    contentDescription = null,
                     tint = AppOnPrimaryColor,
                     modifier = Modifier
                         .fillMaxSize()
@@ -47,7 +84,246 @@ fun BackOrNextButton(onClick: () -> Unit) {
                         .clickable { onClick() }
                 )
             }
+        }
+    }
+}
 
+// Fixme: A very bad way to expose a state
+var globalExposedSearchParam: String? = null
+
+@Composable
+fun SearchBar(
+    autoFocus: Boolean,
+    onSearch: () -> Unit
+) {
+    Box(
+        modifier = Modifier
+            .padding(start = 12.dp, end = 12.dp, bottom = 8.dp)
+            .clip(CircleShape)
+            .background(Color(0XFF423460))
+            .fillMaxWidth()
+            .height(54.dp)
+    ) {
+        var searchInput: String by remember { mutableStateOf("") }
+        val focusRequester = remember { FocusRequester() }
+        val focusManager = LocalFocusManager.current
+
+        TextField(
+            value = searchInput,
+            onValueChange = { newValue ->
+                searchInput = if (newValue.trim().isNotEmpty()) newValue else ""
+                globalExposedSearchParam = searchInput
+                onSearch()
+            },
+            modifier = Modifier
+                .fillMaxSize()
+                .focusRequester(focusRequester = focusRequester),
+            singleLine = true,
+            placeholder = {
+                Text(
+                    text = "Search...",
+                    color = AppOnPrimaryColor.copy(alpha = 0.8F)
+                )
+            },
+            colors = textFieldColors(
+                textColor = Color.White.copy(alpha = 0.78F),
+                backgroundColor = Color.Transparent,
+                disabledTextColor = Color.LightGray,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent
+            ), keyboardOptions = KeyboardOptions(
+                autoCorrect = true,
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Search
+            ),
+            keyboardActions = KeyboardActions(
+                onSearch = {
+                    focusManager.clearFocus()
+                    onSearch()
+                }
+            ),
+            trailingIcon = {
+                LaunchedEffect(Unit) {
+                    if (autoFocus) {
+                        focusRequester.requestFocus()
+                    }
+                }
+                Row() {
+                    AnimatedVisibility(visible = searchInput.trim().isNotEmpty()) {
+                        IconButton(onClick = {
+                            focusManager.clearFocus()
+                            searchInput = ""
+                            globalExposedSearchParam = null
+                        }) {
+                            Icon(
+                                imageVector = Icons.Default.Clear,
+                                tint = AppOnPrimaryColor,
+                                contentDescription = null
+                            )
+                        }
+                    }
+
+                    IconButton(onClick = {
+                        focusManager.clearFocus()
+                        onSearch()
+                    }) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_search),
+                            tint = AppOnPrimaryColor,
+                            contentDescription = null
+                        )
+                    }
+                }
+            }
+        )
+    }
+}
+
+@Composable
+fun BackButton(onClick: () -> Unit) {
+    ConstraintLayout(
+        modifier = Modifier
+            .size(42.dp)
+            .clip(CircleShape)
+            .background(ButtonColor.copy(alpha = 0.78F))
+    ) {
+        val (icon) = createRefs()
+        IconButton(onClick = { onClick() }) {
+            Icon(
+                imageVector = Icons.Rounded.ArrowBack,
+                contentDescription = null,
+                tint = AppOnPrimaryColor,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(6.dp)
+                    .constrainAs(icon) {
+                        top.linkTo(parent.top)
+                        bottom.linkTo(parent.bottom)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                    }
+            )
+        }
+    }
+}
+
+@Composable
+fun SearchResultItem(
+    imdbId: String,
+    title: String,
+    posterImage: Int,
+    genres: List<MovieGenre>,
+    rating: Float,
+    releaseYear: String,
+    showFavorite: Boolean = false,
+    onRemoveFavorite: () -> Unit,
+    onClick: () -> Unit
+) {
+    Box(
+        modifier = Modifier
+            .padding(start = 8.dp, end = 8.dp, bottom = 12.dp)
+            .fillMaxWidth()
+            .height(140.dp)
+            .clip(RoundedCornerShape(8.dp))
+            .background(Color(0XFF423460))
+            .clickable(
+                interactionSource = remember { MutableInteractionSource() },
+                indication = null
+            ) {
+                onClick()
+            }
+    ) {
+        Row {
+            CoilImage(
+                imageModel = posterImage,
+                circularReveal = CircularReveal(duration = 1000),
+                previewPlaceholder = R.drawable.dont_look_up_portrait,
+                shimmerParams = ShimmerParams(
+                    baseColor = AppPrimaryColor,
+                    highlightColor = Color(0XFF423460),
+                    durationMillis = 350,
+                    dropOff = 0.65F,
+                    tilt = 20F
+                ),
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .width(93.33.dp)
+                    .clip(RoundedCornerShape(topStart = 8.dp, bottomStart = 8.dp)),
+                contentScale = ContentScale.Crop,
+                contentDescription = null
+            )
+
+            Column(
+                horizontalAlignment = Alignment.Start,
+                verticalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier
+                    .padding(all = 8.dp)
+                    .fillMaxSize()
+            ) {
+                Text(
+                    text = title,
+                    modifier = Modifier.fillMaxWidth(),
+                    maxLines = 1,
+                    overflow = Ellipsis,
+                    fontWeight = FontWeight.Normal,
+                    textAlign = TextAlign.Center,
+                    fontSize = 18.sp,
+                    color = AppOnPrimaryColor
+                )
+
+                Row(
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    RatingBar(
+                        value = rating,
+                        modifier = Modifier.padding(end = 6.dp),
+                        config = RatingBarConfig()
+                            .style(RatingBarStyle.HighLighted)
+                            .isIndicator(true)
+                            .activeColor(AppOnPrimaryColor)
+                            .hideInactiveStars(false)
+                            .inactiveColor(Color.LightGray)
+                            .stepSize(StepSize.HALF)
+                            .numStars(5)
+                            .size(16.dp)
+                            .padding(4.dp)
+                            .style(RatingBarStyle.HighLighted),
+                        onValueChange = {},
+                        onRatingChanged = {}
+                    )
+                    Text(
+                        text = releaseYear,
+                        fontWeight = FontWeight.Light,
+                        textAlign = TextAlign.Center,
+                        fontSize = 16.sp,
+                        color = AppOnPrimaryColor
+                    )
+
+                    AnimatedVisibility(visible = showFavorite) {
+                        IconButton(onClick = {
+                            onRemoveFavorite()
+                        }) {
+                            Icon(
+                                imageVector = Icons.Filled.Favorite,
+                                tint = AppOnPrimaryColor,
+                                contentDescription = "fav icon"
+                            )
+                        }
+                    }
+                }
+                LazyRow(
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    genres.forEach {
+                        item {
+                            MovieGenreChip(genre = it.name)
+                        }
+                    }
+                }
+
+            }
         }
     }
 }
