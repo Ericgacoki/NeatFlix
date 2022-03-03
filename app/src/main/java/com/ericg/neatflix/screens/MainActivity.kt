02d88879b0f902cfc10e3980a691af0e30759016
Ericgacoki <gacokieric@gmail.com>
2022-03-04 02:41:24 +0300

    display another screen

--- app/src/main/java/com/ericg/neatflix/screens/MainActivity.kt
+++ app/src/main/java/com/ericg/neatflix/screens/MainActivity.kt
@@ -3,7 +3,7 @@ package com.ericg.neatflix.screens
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.ericg.neatflix.screens.SplashScreen
+import androidx.compose.material.ExperimentalMaterialApi
 import com.ericg.neatflix.ui.theme.NeatFlixTheme
 
 class MainActivity : ComponentActivity() {
@@ -12,7 +12,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             NeatFlixTheme {
-                Home()
+                Favorites()
             }
         }
     }
