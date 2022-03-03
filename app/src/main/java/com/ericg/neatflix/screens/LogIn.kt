fca8640152d0a0a1bc7cd20423777e70f130fd30
Ericgacoki <gacokieric@gmail.com>
2022-03-04 02:42:22 +0300

    access the shared composables

--- app/src/main/java/com/ericg/neatflix/screens/LogIn.kt
+++ app/src/main/java/com/ericg/neatflix/screens/LogIn.kt
@@ -24,7 +24,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.ericg.neatflix.R
-import com.ericg.neatflix.composables.IconNext
+import com.ericg.neatflix.sharedComposables.BackOrNextButton
 import com.ericg.neatflix.ui.theme.AppOnPrimaryColor
 
 @Composable
@@ -128,7 +128,7 @@ fun LogInScreen() {
             colors = textFieldColors
         )
 
-        IconNext {
+        BackOrNextButton {
 
         }
     }
