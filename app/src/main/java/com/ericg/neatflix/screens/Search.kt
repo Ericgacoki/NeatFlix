c3f1aadde78fc0bd946023aece3fd4d9fc76bbcb
Ericgacoki <gacokieric@gmail.com>
2022-03-08 02:46:39 +0300

    Add Search screen

---
+++ app/src/main/java/com/ericg/neatflix/screens/Search.kt
@@ -0,0 +1,109 @@
+package com.ericg.neatflix.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.ericg.neatflix.R
+import com.ericg.neatflix.data.MovieGenre
+import com.ericg.neatflix.sharedComposables.BackButton
+import com.ericg.neatflix.sharedComposables.SearchBar
+import com.ericg.neatflix.sharedComposables.SearchResultItem
+import com.ericg.neatflix.sharedComposables.globalExposedSearchParam
+import com.ericg.neatflix.ui.theme.AppOnPrimaryColor
+import com.ericg.neatflix.ui.theme.AppPrimaryColor
+import timber.log.Timber
+
+@Composable
+fun SearchScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(AppPrimaryColor)
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.Start,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .padding(horizontal = 10.dp, vertical = 16.dp)
+                .fillMaxWidth()
+        ) {
+            BackButton {
+
+            }
+
+            Text(
+                text = "Search",
+                modifier = Modifier.padding(start = 50.dp),
+                fontWeight = FontWeight.SemiBold,
+                textAlign = TextAlign.Center,
+                fontSize = 24.sp,
+                color = AppOnPrimaryColor
+            )
+
+        }
+
+        SearchBar(
+            autoFocus = true,
+            onSearch = {
+                Timber.d("Search Param = $globalExposedSearchParam")
+            })
+
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize(),
+            contentPadding = PaddingValues(vertical = 12.dp)
+        ) {
+
+            items(count = 0) {
+                SearchResultItem(
+                    imdbId = "",
+                    title = "Spider-Man far from home",
+                    posterImage = R.drawable.manifest,
+                    genres = listOf(
+                        MovieGenre(1, "Drama"),
+                        MovieGenre(2, "Sci-Fi"),
+                        MovieGenre(3, "Romance"),
+                        MovieGenre(4, "Action"),
+                    ),
+                    rating = 4F,
+                    releaseYear = "2019",
+                    showFavorite = false,
+                    onRemoveFavorite = {},
+                    onClick = {
+
+                    }
+                )
+            }
+
+            item {
+                // FIXME: Display UI status -> loading, no data...
+                if (-1 <= 0) {
+                    Image(
+                        modifier = Modifier
+                            .padding(top = 50.dp)
+                            .fillMaxWidth(),
+                        painter = painterResource(id = R.drawable.no_match_found),
+                        contentDescription = null
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun SearchPrev() {
+    SearchScreen()
+}
