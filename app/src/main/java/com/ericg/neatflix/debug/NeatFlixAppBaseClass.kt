50610b8c7dcc9ee533c1b6b0dbaa8175bd1f03f2
Ericgacoki <gacokieric@gmail.com>
2022-03-24 11:26:34 +0300

    Update base class

--- app/src/main/java/com/ericg/neatflix/debug/NeatFlixAppBaseClass.kt
+++ app/src/main/java/com/ericg/neatflix/debug/NeatFlixAppBaseClass.kt
@@ -1,11 +1,15 @@
 package com.ericg.neatflix.debug
 
 import android.app.Application
+import com.facebook.stetho.Stetho
 import timber.log.Timber
 
-class NeatFlixApp : Application() {
+class NeatFlixAppBaseClass : Application() {
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
+        Stetho.initializeWithDefaults(this)
+
+        Timber.e("Initialized Timber and Stetho")
     }
 }
