323be86e980d559aba3f0b58caae92956b5d4cdf
Ericgacoki <gacokieric@gmail.com>
2022-03-26 02:34:37 +0300

    add lottie loader functions in one dir

---
+++ app/src/main/java/com/ericg/neatflix/sharedComposables/LottieFileLoader.kt
@@ -0,0 +1,60 @@
+package com.ericg.neatflix.sharedComposables
+
+import androidx.annotation.RawRes
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.airbnb.lottie.compose.*
+import timber.log.Timber
+
+@Composable
+fun LottieLoader(
+    modifier: Modifier,
+    @RawRes lottieFile: Int
+) {
+    val composition by rememberLottieComposition(
+        LottieCompositionSpec.RawRes(lottieFile),
+        onRetry = { failCount, exception ->
+            Timber.e("Failed ${failCount}X with exception. Reason: ${exception.localizedMessage}")
+            // stop retrying
+            false
+        }
+    )
+    val progress by animateLottieCompositionAsState(composition)
+    LottieAnimation(
+        composition = composition,
+        progress = progress,
+        modifier = modifier
+    )
+}
+
+@Composable
+fun LoopReverseLottieLoader(
+    modifier: Modifier = Modifier,
+    @RawRes lottieFile: Int,
+    alignment: Alignment = Alignment.Center,
+    enableMergePaths: Boolean = true,
+) {
+    val composition by rememberLottieComposition(spec = LottieCompositionSpec.RawRes(lottieFile))
+    val shouldReverse = remember { mutableStateOf(false) }
+    val anim = rememberLottieAnimatable()
+    if (shouldReverse.value.not())
+        LaunchedEffect(key1 = composition) {
+            anim.animate(composition = composition, speed = 1f)
+            shouldReverse.value = true
+        }
+    if (shouldReverse.value) {
+        LaunchedEffect(composition) {
+            anim.animate(composition = composition, speed = -1f)
+            shouldReverse.value = false
+        }
+    }
+
+    LottieAnimation(
+        composition,
+        anim.value,
+        modifier = modifier,
+        enableMergePaths = remember { enableMergePaths },
+        alignment = alignment
+    )
+}
