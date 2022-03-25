1b7645aa8cafecd6515ad78edb690aff8a1ac6fc
Ericgacoki <gacokieric@gmail.com>
2022-03-25 03:08:25 +0300

    change system bar color using Accompanist

--- app/src/main/java/com/ericg/neatflix/ui/theme/Theme.kt
+++ app/src/main/java/com/ericg/neatflix/ui/theme/Theme.kt
@@ -5,6 +5,8 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorPalette = darkColors(
     primary = AppOnPrimaryColor,
@@ -33,6 +35,12 @@ fun NeatFlixTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composab
         DarkColorPalette
     } else {
         LightColorPalette
+    }
+    val systemUiController = rememberSystemUiController()
+    SideEffect {
+        systemUiController.setSystemBarsColor(
+            color = AppPrimaryColor
+        )
     }
 
     MaterialTheme(
