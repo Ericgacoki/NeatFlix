37fe18070e459a588b7baaac8dcf42844998c037
Ericgacoki <gacokieric@gmail.com>
2022-03-27 04:35:34 +0300

    Add Hilt Module

--- app/src/main/java/com/ericg/neatflix/di/AppModule.kt
+++ app/src/main/java/com/ericg/neatflix/di/AppModule.kt
@@ -1,7 +1,16 @@
 package com.ericg.neatflix.di
 
-import android.app.Application
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
+@Module
+@InstallIn(SingletonComponent::class)
 object AppModule {
 
+    @Singleton
+    @Provides
+    fun provideTestTitle() = "This Title was Injected \uD83D\uDC89"
 }
