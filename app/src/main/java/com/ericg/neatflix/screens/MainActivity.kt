071d2cb851ccb5ecece630b08fdc4e5701ceeb7f
Ericgacoki <gacokieric@gmail.com>
2022-03-27 04:36:30 +0300

    Add Hilt Entrypoint

--- app/src/main/java/com/ericg/neatflix/screens/MainActivity.kt
+++ app/src/main/java/com/ericg/neatflix/screens/MainActivity.kt
@@ -3,16 +3,17 @@ package com.ericg.neatflix.screens
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.ExperimentalMaterialApi
 import com.ericg.neatflix.ui.theme.NeatFlixTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             NeatFlixTheme {
-                Home()
+                MovieDetails()
             }
         }
     }
