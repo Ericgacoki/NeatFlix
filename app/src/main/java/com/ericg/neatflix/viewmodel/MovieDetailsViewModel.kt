e7a582a16675423091d8b79f56912f236fdad56d
Ericgacoki <gacokieric@gmail.com>
2022-03-27 04:40:11 +0300

    Add movie details Hilt viewModel Inject title in the constructor

---
+++ app/src/main/java/com/ericg/neatflix/viewmodel/MovieDetailsViewModel.kt
@@ -0,0 +1,22 @@
+package com.ericg.neatflix.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.ericg.neatflix.data.MovieGenre as Genre
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MovieDetailsViewModel @Inject constructor(private val title: String) : ViewModel() {
+    fun getMovieTitle(): String = title
+
+    fun getMovieGenres(
+        movieId: Int = 0
+    ): List<Genre>{
+        return listOf(
+            Genre(movieId,"Sci-fi"),
+            Genre(movieId,"Porn \uD83C\uDF51"),
+            Genre(movieId,"Drama"),
+            Genre(movieId,"Fantasy"),
+            Genre(movieId,"Adventure"))
+    }
+}
