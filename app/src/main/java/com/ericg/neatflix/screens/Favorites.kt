c27f2a0e988afaedb09c6e56a5b0b8d8907ec967
Ericgacoki <gacokieric@gmail.com>
2022-03-28 01:02:06 +0300

    add favorites navigator

--- app/src/main/java/com/ericg/neatflix/screens/Favorites.kt
+++ app/src/main/java/com/ericg/neatflix/screens/Favorites.kt
@@ -3,10 +3,14 @@ package com.ericg.neatflix.screens
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -14,41 +18,65 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.ericg.neatflix.R
 import com.ericg.neatflix.data.MovieGenre
+import com.ericg.neatflix.screens.destinations.HomeDestination
+import com.ericg.neatflix.screens.destinations.MovieDetailsDestination
 import com.ericg.neatflix.sharedComposables.BackButton
 import com.ericg.neatflix.sharedComposables.SearchBar
 import com.ericg.neatflix.sharedComposables.SearchResultItem
 import com.ericg.neatflix.sharedComposables.globalExposedSearchParam
 import com.ericg.neatflix.ui.theme.AppOnPrimaryColor
 import com.ericg.neatflix.ui.theme.AppPrimaryColor
+import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import timber.log.Timber
 
+@Destination
 @Composable
-fun Favorites() {
+fun Favorites(
+    navigator: DestinationsNavigator
+) {
     Column(
         modifier = Modifier
             .fillMaxSize()
             .background(AppPrimaryColor)
     ) {
         Row(
-            horizontalArrangement = Arrangement.Start,
+            horizontalArrangement = Arrangement.SpaceBetween,
             verticalAlignment = Alignment.CenterVertically,
             modifier = Modifier
                 .padding(horizontal = 10.dp, vertical = 16.dp)
                 .fillMaxWidth()
         ) {
+            val focusManager = LocalFocusManager.current
             BackButton {
-
+                focusManager.clearFocus()
+                navigator.navigateUp()
             }
 
             Text(
                 text = "My Favorites",
-                modifier = Modifier.padding(start = 50.dp),
+                // modifier = Modifier.padding(start = 42.dp),
                 fontWeight = FontWeight.SemiBold,
                 textAlign = TextAlign.Center,
                 fontSize = 24.sp,
                 color = AppOnPrimaryColor
             )
 
+            IconButton(
+                onClick = {
+                    focusManager.clearFocus()
+                    navigator.navigate(HomeDestination()) {
+                        this.launchSingleTop = true
+                        this.restoreState
+                    }
+                }) {
+                Icon(
+                    modifier = Modifier.size(26.dp),
+                    painter = painterResource(id = R.drawable.ic_home),
+                    tint = AppOnPrimaryColor,
+                    contentDescription = "home icon"
+                )
+            }
         }
 
         SearchBar(
@@ -57,6 +85,7 @@ fun Favorites() {
                 Timber.d("Search Param = $globalExposedSearchParam")
             })
 
+        val focusManager = LocalFocusManager.current
         LazyColumn(
             modifier = Modifier
                 .fillMaxSize(),
@@ -77,10 +106,13 @@ fun Favorites() {
                     releaseYear = "2019",
                     showFavorite = true,
                     onRemoveFavorite = {
-                        Timber.d("Removing favorite...")
+                        Timber.d("Remove from Room DB")
                     },
                     onClick = {
-                        Timber.d("Navigating to details screen...")
+                        focusManager.clearFocus()
+                        navigator.navigate(MovieDetailsDestination(it)) {
+                            this.launchSingleTop = true
+                        }
                     }
                 )
             }
@@ -91,5 +123,5 @@ fun Favorites() {
 @Preview
 @Composable
 fun FavoritesPrev() {
-    Favorites()
+    // Favorites()
 }
