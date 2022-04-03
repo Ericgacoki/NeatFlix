7e8a26c2704c4daf37a6ec5a47c9c917319d0779
Ericgacoki <gacokieric@gmail.com>
2022-04-03 15:11:18 +0300

    add genre model class

---
+++ app/src/main/java/com/ericg/neatflix/model/Genre.kt
@@ -0,0 +1,13 @@
+package com.ericg.neatflix.model
+
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Genre(
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("name")
+    val name: String
+): Parcelable
