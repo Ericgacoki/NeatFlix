5d9ab1cbc480e3cb00cb5b5bad34f710da5c5bb3
Ericgacoki <gacokieric@gmail.com>
2022-04-03 15:11:47 +0300

    add constants

---
+++ app/src/main/java/com/ericg/neatflix/util/Constants.kt
@@ -0,0 +1,9 @@
+package com.ericg.neatflix.util
+
+import com.ericg.neatflix.R
+
+object Constants {
+    const val API_KEY = R.string.NEATFLIX_API_KEY
+    const val BASE_URL = "https://api.themoviedb.org/3/"
+    const val IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500/"
+}
