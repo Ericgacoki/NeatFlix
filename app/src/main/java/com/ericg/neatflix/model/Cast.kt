4dcf597e81bb0a5fea2ca8fe1cb145b86256a195
Ericgacoki <gacokieric@gmail.com>
2022-04-03 15:12:01 +0300

    add cast model class

---
+++ app/src/main/java/com/ericg/neatflix/model/Cast.kt
@@ -0,0 +1,27 @@
+package com.ericg.neatflix.model
+
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Cast(
+    @SerializedName("adult")
+    val adult: Boolean,
+    @SerializedName("gender")
+    val gender: Int?,
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("known_for_department")
+    val department: String,
+    @SerializedName("name")
+    val name: String,
+    @SerializedName("original_name")
+    val originalName: String,
+    @SerializedName("profile_path")
+    val profilePath: String?,
+    @SerializedName("cast_id")
+    val castId: Int,
+    @SerializedName("character")
+    val character: String
+): Parcelable
