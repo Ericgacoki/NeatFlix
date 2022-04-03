7cb1ff93da68d7f029cf90d5e9c214cc290a10fe
Ericgacoki <gacokieric@gmail.com>
2022-04-03 15:10:33 +0300

    add movie repository

---
+++ app/src/main/java/com/ericg/neatflix/data/response/MoviesResponse.kt
@@ -0,0 +1,15 @@
+package com.ericg.neatflix.data.response
+
+import com.ericg.neatflix.model.Movie
+import com.google.gson.annotations.SerializedName
+
+data class MoviesResponse(
+    @SerializedName("page")
+    val page: Int,
+    @SerializedName("results")
+    val results: List<Movie>,
+    @SerializedName("total_pages")
+    val totalPages: Int,
+    @SerializedName("total_results")
+    val totalResults: Int
+)
