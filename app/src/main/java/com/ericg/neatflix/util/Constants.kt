9b7c40dba63a7a8379f2a56f930cbb9929098c6a
Ericgacoki <gacokieric@gmail.com>
2022-04-05 02:07:27 +0300

    add base image urls

--- app/src/main/java/com/ericg/neatflix/util/Constants.kt
+++ app/src/main/java/com/ericg/neatflix/util/Constants.kt
@@ -4,5 +4,6 @@ import com.ericg.neatflix.R
 
 object Constants {
     const val BASE_URL = "https://api.themoviedb.org/3/"
-    const val IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500/"
+    const val BASE_BACKDROP_IMAGE_URL = "https://image.tmdb.org/t/p/w780/"
+    const val BASE_POSTER_IMAGE_URL = "https://image.tmdb.org/t/p/w500/"
 }
