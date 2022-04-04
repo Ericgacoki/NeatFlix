431fd264898b7260cbde208b95dba9901d68bad1
Ericgacoki <gacokieric@gmail.com>
2022-04-04 15:15:59 +0300

    add retrofit instance and repo dependencies for injection

--- app/src/main/java/com/ericg/neatflix/di/AppModule.kt
+++ app/src/main/java/com/ericg/neatflix/di/AppModule.kt
@@ -1,16 +1,57 @@
 package com.ericg.neatflix.di
 
+import com.ericg.neatflix.data.repository.GenreRepository
+import com.ericg.neatflix.data.repository.MoviesRepository
+import com.ericg.neatflix.model.APIService
+import com.ericg.neatflix.util.Constants.BASE_URL
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+    @Singleton
+    @Provides
+    fun providesLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+    }
 
     @Singleton
     @Provides
-    fun provideTestTitle() = "This Title was Injected \uD83D\uDC89"
+    fun providesOkHttpClient(httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .callTimeout(15, TimeUnit.SECONDS)
+            .connectTimeout(15, TimeUnit.SECONDS)
+            .writeTimeout(15, TimeUnit.SECONDS)
+            .readTimeout(15, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun providesAPI(okHttpClient: OkHttpClient): APIService {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+            .create(APIService::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun provideMoviesRepository(api: APIService) = MoviesRepository(api = api)
+
+    @Singleton
+    @Provides
+    fun providesGenresRepository(api: APIService) = GenreRepository(api)
 }
