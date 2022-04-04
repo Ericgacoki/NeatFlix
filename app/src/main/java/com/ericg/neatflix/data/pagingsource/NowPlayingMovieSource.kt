ac9b0520f438678aca6cb2f69289ea563e4576a2
Ericgacoki <gacokieric@gmail.com>
2022-04-04 15:02:52 +0300

    add now playing movies source

---
+++ app/src/main/java/com/ericg/neatflix/data/pagingsource/NowPlayingMovieSource.kt
@@ -0,0 +1,29 @@
+package com.ericg.neatflix.data.pagingsource
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.ericg.neatflix.model.APIService
+import com.ericg.neatflix.model.Movie
+import retrofit2.HttpException
+import java.io.IOException
+
+class NowPlayingMovieSource(private val api: APIService) : PagingSource<Int, Movie>() {
+    override fun getRefreshKey(state: PagingState<Int, Movie>): Int? = state.anchorPosition
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Movie> {
+        return try {
+            val nextPage = params.key ?: 1
+            val nowPlayingMovies = api.getNowPlayingMovies(page = nextPage)
+
+            LoadResult.Page(
+                data = nowPlayingMovies.results,
+                prevKey = if (nextPage == 1) null else nextPage - 1,
+                nextKey = if (nowPlayingMovies.results.isEmpty()) null else nowPlayingMovies.page + 1
+            )
+        } catch (e: IOException) {
+            return LoadResult.Error(e)
+        } catch (e: HttpException) {
+            return LoadResult.Error(e)
+        }
+    }
+}
