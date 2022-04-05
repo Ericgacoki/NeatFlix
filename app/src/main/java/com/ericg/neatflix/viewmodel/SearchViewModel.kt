d2a95dec1d64b11b3a561b616e973a6af75e611f
Ericgacoki <gacokieric@gmail.com>
2022-04-05 20:08:01 +0300

    add search viewmodel

---
+++ app/src/main/java/com/ericg/neatflix/viewmodel/SearchViewModel.kt
@@ -0,0 +1,38 @@
+package com.ericg.neatflix.viewmodel
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import com.ericg.neatflix.data.repository.SearchRepository
+import com.ericg.neatflix.model.APIService
+import com.ericg.neatflix.model.Movie
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emptyFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class SearchViewModel @Inject constructor(
+    private val searchRepository: SearchRepository
+) : ViewModel() {
+    private var _searchMovies = mutableStateOf<Flow<PagingData<Movie>>>(emptyFlow())
+    val searchMoviesState: State<Flow<PagingData<Movie>>> = _searchMovies
+
+    var searchParam = mutableStateOf("")
+
+    fun searchMovie() {
+        viewModelScope.launch {
+            _searchMovies.value = if (searchParam.value.isNotEmpty()) {
+                searchRepository.multiSearch(searchParams = searchParam.value)
+                    .cachedIn(viewModelScope)
+            } else {
+                searchRepository.multiSearch(searchParams = searchParam.value)
+                    .cachedIn(viewModelScope)
+            }
+        }
+    }
+}
