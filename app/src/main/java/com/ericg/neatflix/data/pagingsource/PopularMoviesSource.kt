495ce566140444d31d1f45d5e21fbf0378d661f2
Ericgacoki <gacokieric@gmail.com>
2022-04-05 20:09:08 +0300

    add popular movies source

---
+++ app/src/main/java/com/ericg/neatflix/data/pagingsource/PopularMoviesSource.kt
@@ -0,0 +1,28 @@
+package com.ericg.neatflix.data.pagingsource
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.ericg.neatflix.model.APIService
+import com.ericg.neatflix.model.Movie
+import retrofit2.HttpException
+import java.io.IOException
+
+class PopularMoviesSource(private val api: APIService) : PagingSource<Int, Movie>() {
+    override fun getRefreshKey(state: PagingState<Int, Movie>): Int? = state.anchorPosition
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Movie> {
+        return try {
+            val nextPage = params.key ?: 1
+            val popularMovies = api.getPopularMovies(page = nextPage)
+            LoadResult.Page(
+                data = popularMovies.results,
+                prevKey = if (nextPage == 1) null else nextPage - 1,
+                nextKey = if (popularMovies.results.isEmpty()) null else popularMovies.page + 1
+            )
+        } catch (e: IOException) {
+            return LoadResult.Error(e)
+        } catch (e: HttpException) {
+            return LoadResult.Error(e)
+        }
+    }
+}
