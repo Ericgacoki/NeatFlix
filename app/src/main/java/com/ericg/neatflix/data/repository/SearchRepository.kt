deeeb9f66cf7491d6ab8fe1706139c68365b4026
Ericgacoki <gacokieric@gmail.com>
2022-04-05 20:08:13 +0300

    add search repository

---
+++ app/src/main/java/com/ericg/neatflix/data/repository/SearchRepository.kt
@@ -0,0 +1,23 @@
+package com.ericg.neatflix.data.repository
+
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import com.ericg.neatflix.data.pagingsource.SearchMovieSource
+import com.ericg.neatflix.model.APIService
+import com.ericg.neatflix.model.Movie
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class SearchRepository @Inject constructor(
+    private val api: APIService
+) {
+    fun multiSearch(searchParams: String): Flow<PagingData<Movie>> {
+        return Pager(
+            config = PagingConfig(enablePlaceholders = false, pageSize = 20),
+            pagingSourceFactory = {
+                SearchMovieSource(api = api, searchParams = searchParams)
+            }
+        ).flow
+    }
+}
