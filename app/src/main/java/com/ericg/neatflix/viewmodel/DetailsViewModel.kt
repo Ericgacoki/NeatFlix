45ba91b15046185e5640d02629ec4706e5d31e66
Ericgacoki <gacokieric@gmail.com>
2022-04-18 02:31:44 +0300

    add similar movies viewModel

--- app/src/main/java/com/ericg/neatflix/viewmodel/DetailsViewModel.kt
+++ app/src/main/java/com/ericg/neatflix/viewmodel/DetailsViewModel.kt
@@ -1,10 +1,29 @@
 package com.ericg.neatflix.viewmodel
 
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import com.ericg.neatflix.data.repository.MoviesRepository
+import com.ericg.neatflix.model.Movie
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emptyFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class DetailsViewModel @Inject constructor() : ViewModel() {
+class DetailsViewModel @Inject constructor(val repository: MoviesRepository) : ViewModel() {
+    private var _similarMovies = mutableStateOf<Flow<PagingData<Movie>>>(emptyFlow())
+    val similarMovies: State<Flow<PagingData<Movie>>> = _similarMovies
 
+    fun getSimilarMovies(movieId: Int) {
+        viewModelScope.launch {
+            val result = repository.getSimilarMovies(movieId)
+            result.let {
+                _similarMovies.value = it
+            }
+        }
+    }
 }
