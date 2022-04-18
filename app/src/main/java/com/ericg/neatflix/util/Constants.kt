7336294f9de11a7fe84a07a20e02c27ec63819e4
Ericgacoki <gacokieric@gmail.com>
2022-04-19 01:12:16 +0300

    remove unused import

--- app/src/main/java/com/ericg/neatflix/util/Constants.kt
+++ app/src/main/java/com/ericg/neatflix/util/Constants.kt
@@ -1,7 +1,5 @@
 package com.ericg.neatflix.util
 
-import com.ericg.neatflix.R
-
 object Constants {
     const val BASE_URL = "https://api.themoviedb.org/3/"
     const val BASE_BACKDROP_IMAGE_URL = "https://image.tmdb.org/t/p/w780/"
