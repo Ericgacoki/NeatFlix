33f30dc4ece5332c6cd8025e232f9844251af947
Ericgacoki <gacokieric@gmail.com>
2022-05-07 17:20:12 +0300

    remove unused cast attributes

--- app/src/main/java/com/ericg/neatflix/model/Cast.kt
+++ app/src/main/java/com/ericg/neatflix/model/Cast.kt
@@ -5,27 +5,12 @@ import com.google.gson.annotations.SerializedName
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
+/**Unused attributes were not included here*/
 data class Cast(
-    @SerializedName("adult")
-    val adult: Boolean,
-    @SerializedName("gender")
-    val gender: Int?,
-    @SerializedName("id")
-    val id: Int,
     @SerializedName("known_for_department")
     val department: String,
     @SerializedName("name")
     val name: String,
-    @SerializedName("original_name")
-    val originalName: String,
     @SerializedName("profile_path")
-    val profilePath: String?,
-    @SerializedName("cast_id")
-    val castId: Int,
-    @SerializedName("character")
-    val character: String
+    val profilePath: String?
 ): Parcelable
-
-data class CastDemo(
-   val image: Int
-)
