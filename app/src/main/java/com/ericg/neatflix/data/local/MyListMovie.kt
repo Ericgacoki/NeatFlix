133a731289435cc0d679faf0e37a5ea822b0fc7e
Ericgacoki <gacokieric@gmail.com>
2022-05-10 12:15:07 +0300

    add watchlist movie

---
+++ app/src/main/java/com/ericg/neatflix/data/local/MyListMovie.kt
@@ -0,0 +1,14 @@
+package com.ericg.neatflix.data.local
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "watch_list_table")
+data class MyListMovie(
+    @PrimaryKey val mediaId: Int,
+    val imagePath: String?,
+    val title: String,
+    val releaseDate: String,
+    val rating: Double,
+    // @Embedded val genre: List<Genre>?
+)
