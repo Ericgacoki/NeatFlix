0ac5003837b928bc05880bc099a16f3a93266997
Ericgacoki <gacokieric@gmail.com>
2022-06-01 23:14:29 +0300

    rename APIService to ApiService

--- app/src/main/java/com/ericg/neatflix/data/repository/SearchRepository.kt
+++ app/src/main/java/com/ericg/neatflix/data/repository/SearchRepository.kt
@@ -3,15 +3,14 @@ package com.ericg.neatflix.data.repository
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
-import com.ericg.neatflix.data.remote.APIService
-import com.ericg.neatflix.model.Film
+import com.ericg.neatflix.data.remote.ApiService
 import com.ericg.neatflix.model.Search
 import com.ericg.neatflix.paging.SearchFilmSource
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class SearchRepository @Inject constructor(
-    private val api: APIService
+    private val api: ApiService
 ) {
     fun multiSearch(searchParams: String, includeAdult: Boolean): Flow<PagingData<Search>> {
         return Pager(
