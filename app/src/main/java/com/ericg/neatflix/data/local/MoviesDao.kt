effb88fe8e0c0de2b507499943c730f426775a48
Ericgacoki <gacokieric@gmail.com>
2023-02-21 11:08:14 +0300

    change project root name

--- app/src/main/java/com/ericg/neatflix/data/local/MoviesDao.kt
+++ app/src/main/java/com/ericg/neatflix/data/local/MoviesDao.kt
@@ -1,4 +1,4 @@
-package com.ericg.neatfreaks.data.local
+package com.ericg.neatflix.data.local
 
 import androidx.room.*
 import kotlinx.coroutines.flow.Flow
--- app/src/main/java/com/ericg/neatflix/data/remote/response/MultiSearchResponse.kt
+++ app/src/main/java/com/ericg/neatflix/data/remote/response/MultiSearchResponse.kt
@@ -1,6 +1,6 @@
-package com.ericg.neatfreaks.data.remote.response
+package com.ericg.neatflix.data.remote.response
 
-import com.ericg.neatfreaks.model.Search
+import com.ericg.neatflix.model.Search
 import com.google.gson.annotations.SerializedName
 
 class MultiSearchResponse(
